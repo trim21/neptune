489fff30d93be02d40749cac21107f5015e80fe6
Trim21 <trim21.me@gmail.com>
2024-07-05 07:29:32 +0800

    chore: fix license header

--- internal/pkg/null/null_test.go
+++ internal/pkg/null/null_test.go
@@ -1,8 +1,6 @@
 // Copyright 2024 trim21 <trim21.me@gmail.com>
 // SPDX-License-Identifier: GPL-3.0-only
 
-// SPDX-License-Identifier: GPL-3.0-only
-
 package null_test
 
 import (
--- tools/scan-license-header.py
+++ tools/scan-license-header.py
@@ -1,15 +1,44 @@
 import glob
 import sys
+import re
+import posixpath
+
+pattern = re.compile(r"// SPDX-License-Identifier: (.*)\n")
 
 
 def main():
     ret = 0
     for file in glob.iglob("**/*.go", recursive=True):
         with open(file, "r") as f:
-            lines = f.read()
-            if "// SPDX-License-Identifier: " not in lines:
-                print("missing license header:", file)
-                ret = 1
+            lines = f.readlines()
+
+        file = file.replace("\\", "/")
+
+        matches = []
+        for lino, line in enumerate(lines):
+            lino = lino + 1
+            m = pattern.findall(line)
+            if m:
+                matches.append((lino, m[0]))
+
+        if len(matches) == 0:
+            print(f"{file}:1", "missing license header")
+            ret = 1
+            continue
+
+        if len(matches) > 1:
+            for lino, m in matches:
+                print(f"{file}:{lino}", "found multiple license headers:", m)
+            ret = 1
+            continue
+
+        lino, m = matches[0]
+
+        if m not in ["GPL-3.0-only", "MIT", "MPL-2.0", "Apache-2.0"]:
+            print(f"{file}:{lino}", "invalid license:", m)
+            ret = 1
+            continue
+
     sys.exit(ret)
 
 
