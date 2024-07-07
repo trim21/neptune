b8e9041e79a4138685c2c735e66993978dcfa8fe
Trim21 <trim21.me@gmail.com>
2024-07-08 02:31:53 +0800

    ci: support arm64 docker image

--- etc/keep-arch-file.bash
+++ etc/keep-arch-file.bash
@@ -3,9 +3,10 @@
 arch=$(dpkg --print-architecture)
 
 if [[ $arch == "amd64" ]]; then
-  mv /dist/tyr-amd64 /dist/tyr
+  mv /dist/tyr_linux_amd64 /dist/tyr
 elif [[ $arch == "arm64" ]]; then
-  mv /dist/tyr-arm64 /dist/tyr
+  mv /dist/tyr_linux_arm64 /dist/tyr
 else
+  echo unexpected arch "${arch}"
   exit 1
 fi
