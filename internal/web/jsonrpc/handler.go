ae74a6267242e357905e159c8813e210bc15580d
trim21 <i@trim21.me>
2026-06-12 21:49:47 +0800

    fix: enhance error message in invoke method for better debugging

--- internal/web/jsonrpc/handler.go
+++ internal/web/jsonrpc/handler.go
@@ -232,7 +232,7 @@ func (h *Handler) invoke(ctx context.Context, req Request, resp *Response) {
 	}
 
 	if err := m.useCase.Interact(ctx, input, output); err != nil {
-		h.errResp(resp, "operation failed", CodeInternalError, err)
+		h.errResp(resp, fmt.Sprintf("operation failed: %s", err.Error()), CodeInternalError, err)
 
 		return
 	}
