0202bc2f5ea4c53237d8d93e438ffd9cd9f8e2b6
trim21 <i@trim21.me>
2026-07-07 22:07:21 +0800

    fix: missing init for corruptedPieces

--- internal/download/new.go
+++ internal/download/new.go
@@ -70,9 +70,10 @@ func New(sess *session.Session, m *metainfo.MetaInfo, info meta.Info, basePath s
 		ioDownloadRate:    flowrate.New(time.Second, 5*time.Second),
 		pieceUploadRate:   flowrate.New(time.Second, 5*time.Second),
 
-		peers:          xsync.NewMap[uint64, *Peer](),
-		connectedAddrs: xsync.NewMap[netip.AddrPort, *Peer](),
-		peerList:       newPeerList(nil), // d set below
+		peers:           xsync.NewMap[uint64, *Peer](),
+		connectedAddrs:  xsync.NewMap[netip.AddrPort, *Peer](),
+		peerList:        newPeerList(nil), // d set below
+		corruptedPieces: make(map[uint32]int),
 
 		picker: newPiecePicker(info, completedBm),
 
